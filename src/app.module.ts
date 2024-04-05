98b811653c6e02d236245994b969580e7acbee36
Johann C <joe7hann@proton.me>
2024-04-05 15:59:11 -0500

    Install pg, Config TypeORM .forRoot

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //PROD: false
+    }),
   ],
   controllers: [],
   providers: [],
