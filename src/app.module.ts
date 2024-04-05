3fd2ee39a26ef8821172da13a208ee7102bf25fd
Johann C <joe7hann@proton.me>
2024-04-05 15:45:30 -0500

    Config TypeORM in the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRoot(),
   ],
   controllers: [],
   providers: [],
